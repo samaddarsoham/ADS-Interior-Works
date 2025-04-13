4070e9c8b97b0ba457ad34654dbd4bd73a46d976
Soham <samaddarsoham@gmail.com>
2025-04-13 09:50:17 +0530

    Remove unused files and dependencies (postcss.config.cjs and TypeScript types)

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ADS-Interior-Works/', // must match your repo name
   plugins: [
     react(),
   ],
